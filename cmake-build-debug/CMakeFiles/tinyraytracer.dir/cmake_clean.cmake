bc2c2143172f956912b18b51d045f2d210b2e629
cocosh03 <corentin.lexa@gmail.com>
2019-02-04 21:11:18 +0100

    test

---
+++ cmake-build-debug/CMakeFiles/tinyraytracer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tinyraytracer.dir/model.cpp.obj"
+  "CMakeFiles/tinyraytracer.dir/tinyraytracer.cpp.obj"
+  "tinyraytracer.pdb"
+  "tinyraytracer.exe"
+  "tinyraytracer.exe.manifest"
+  "libtinyraytracer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tinyraytracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
